3d1655dacb0ca96b35411ec5683204de96db62fe
StaleButValid <max@edgecomet.com>
2026-01-04 21:31:07 +0200

    log analyzer link and robots txt

--- src/Templates/partials/nav.php
+++ src/Templates/partials/nav.php
@@ -7,6 +7,7 @@
         <div class="nav-links">
             <a href="/#about" class="nav-link">About</a>
             <a href="/#how-it-works" class="nav-link">How It Works</a>
+            <a href="/logs/" class="nav-link">Log Analyzer</a>
             <a href="https://github.com/EdgeComet/openseotest" class="nav-link" target="_blank" rel="noopener">GitHub</a>
         </div>
     </div>
